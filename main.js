ef4cd0bb22b56f2776c3e5839e039435ca1d45fc
vivavivan <vivan.chandrasekhara@gmail.com>
2022-01-27 19:42:09 +0100

    Code Challenge 8 hehe boi

--- main.js
+++ main.js
@@ -4,21 +4,32 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale:false },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0, onSale:true },  
+            ],
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
+        updateVariant(index) {
+            this.selectedVariant = index 
+        },
+    },
+    computed: {
+        title()  {
+            return this.brand + ' ' + this.product + ' ' + (this.variants[this.selectedVariant].onSale? 'is on Sale' : '')
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
     }
+
 })
